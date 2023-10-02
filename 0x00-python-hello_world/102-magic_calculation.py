1d9d09b28de0cd4665ad866924e84c5897a323ad
danielntow <danielntow@gamil.com>
2023-10-02 15:46:30 +0000

    python

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,12 +1,3 @@
+#!/usr/bin/env python3
 def magic_calculation(a, b):
-    """Performs the same calculation as the following Python bytecode:
-
-    3           0 LOAD_CONST               1 (98)
-                3 LOAD_FAST                0 (a)
-                6 LOAD_FAST                1 (b)
-                9 BINARY_POWER
-               10 BINARY_ADD
-               11 RETURN_VALUE
-    """
-
     return 98 ** a + b
