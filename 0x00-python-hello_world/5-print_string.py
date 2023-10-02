76205ef2f8e81db9aea3a2ab92adf2d9444ca9d6
danielntow <danielntow@gamil.com>
2023-10-02 13:24:34 +0000

    python

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-# Print the string 3 times followed by a new line
-# Print the first 9 characters of the string followed by a new line
-print(f"{str * 3}\n{str[:9]}")
+print(str * 3)
+print(str[:9])
