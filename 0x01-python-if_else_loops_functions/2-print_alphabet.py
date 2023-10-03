3c8b1d9398cb614bad51926467ce1bc23f578ffc
danielntow <danielntow@gmail.com>
2023-10-03 19:56:43 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Program to print the ASCII alphabet in lowercase without a new line
+def print_alphabet():
+    for i in range(ord('a'), ord('z') + 1):
+        print("{}".format(chr(i)), end='')
+    print()
+
+
+# Call the function to print the alphabet
+print_alphabet()
