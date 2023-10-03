a6f94ccf8a54b6067b7c0f94960e81e2c1208353
danielntow <danielntow@gmail.com>
2023-10-03 21:46:47 +0000

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for alphabet in range(97, 123):
-    if alphabet == 101 or alphabet == 113:
-        continue
-    print("{}".format(chr(alphabet)), end="")
-
+# Loop to print the alphabet without 'q' and 'e' using string format
+for char in 'abcdefghijklmnopqrstuvwxyz':
+    if char != 'q' and char != 'e':
+        print("{}".format(char), end='')
 
 '''
 for i in range(97, 123):
