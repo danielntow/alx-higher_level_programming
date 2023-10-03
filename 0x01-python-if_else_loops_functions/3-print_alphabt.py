4e3b8b1382a07ba690dd332b4a57ea15950caee0
danielntow <danielntow@gmail.com>
2023-10-03 20:41:24 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# Loop to print the alphabet without 'q' and 'e' using string format
+for char in 'abcdefghijklmnopqrstuvwxyz':
+    if char != 'q' and char != 'e':
+        print("{}".format(char), end='')
