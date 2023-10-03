7b9ce4d3b12c271cbfe992579bb6a1efd1c57b53
danielntow <danielntow@gmail.com>
2023-10-03 21:47:33 +0000

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,22 +4,19 @@ for char in 'abcdefghijklmnopqrstuvwxyz':
     if char != 'q' and char != 'e':
         print("{}".format(char), end='')
 
-'''
-for i in range(97, 123):
-    if i == ord('q') or i == ord('e'):
-        continue
-    print("{}".format(chr(i)), end='')
+# for i in range(97, 123):
+#     if i == ord('q') or i == ord('e'):
+#         continue
+#     print("{}".format(chr(i)), end='')
 
 
-for i in range(ord('a'), ord('z') + 1):
-    if i == ord('q') or i == ord('e'):
-        continue
-    print("{}".format(chr(i)), end='')
+# for i in range(ord('a'), ord('z') + 1):
+#     if i == ord('q') or i == ord('e'):
+#         continue
+#     print("{}".format(chr(i)), end='')
 
 
-# Loop to print the alphabet without 'q' and 'e' using string format
-for char in 'abcdefghijklmnopqrstuvwxyz':
-    if char != 'q' and char != 'e':
-        print("{}".format(char), end='')
-
-'''
+# # Loop to print the alphabet without 'q' and 'e' using string format
+# for char in 'abcdefghijklmnopqrstuvwxyz':
+#     if char != 'q' and char != 'e':
+#         print("{}".format(char), end='')
