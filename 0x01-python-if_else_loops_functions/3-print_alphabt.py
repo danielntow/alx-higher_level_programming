7ac06c106d5ca7d38bef5d3b3de63a8733442e22
danielntow <danielntow@gmail.com>
2023-10-03 21:36:00 +0000

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+for i in range(97, 123):
+    if i == ord('q') or i == ord('e'):
+        continue
+    print("{}".format(chr(i)), end='')
+'''
+for i in range(ord('a'), ord('z') + 1):
+    if i == ord('q') or i == ord('e'):
+        continue
+    print("{}".format(chr(i)), end='')
+
+
 # Loop to print the alphabet without 'q' and 'e' using string format
 for char in 'abcdefghijklmnopqrstuvwxyz':
     if char != 'q' and char != 'e':
         print("{}".format(char), end='')
+
+'''
