87b76d64dc2c1aa347ce6d1211b2102f63a6a836
danielntow <danielntow@gmail.com>
2023-10-03 20:02:42 +0000

    python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+output = ""
 
-# Using a list comprehension to generate the alphabet
-alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-
-# Printing the alphabet without newlines
-print(f"{''.join(alphabet)}")
+for i in range(97, 123):
+    output += "{}".format(chr(i))
+print(output)
