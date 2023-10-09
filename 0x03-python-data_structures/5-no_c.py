b66547df490601ee31999185fad7a9c9328c1217
danielntow <danielntow@gmail.com>
2023-10-09 10:30:32 +0000

    python

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    result_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result_string += char
+    return result_string
