5f3374c85654f5650a904fbbc8d12f7406223bbe
danielntow <danielntow@gmail.com>
2023-10-11 13:02:52 +0000

    python

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary_mine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
+
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
+
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        value = a_dictionary[key]
+        print(f"{key}: {value}")
