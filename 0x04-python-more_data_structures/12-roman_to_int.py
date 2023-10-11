2e21e74a049525d5a533fe27ef3fbb3c1e9f5281
danielntow <danielntow@gmail.com>
2023-10-11 17:15:33 +0000

    python

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import roman
+
+
+def roman_to_ints(roman):
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman):
+        value = roman_dict[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
+
+def roman_to_int(roman_string):
+    result = roman.roman_to_int(roman_string)
+    return result
---
+++ 0x04-python-more_data_structures/try_roman.py
@@ -0,0 +1,3 @@
+import roman
+result = roman.int_to_roman(947)
+print(result)
