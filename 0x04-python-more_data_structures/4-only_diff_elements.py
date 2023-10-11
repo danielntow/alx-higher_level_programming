5ac4bb16f0be10aece0fa84e8731c1e3ea89811e
danielntow <danielntow@gmail.com>
2023-10-11 12:18:16 +0000

    python

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,14 +1,3 @@
+#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     return set_1 ^ set_2
-
-
-def only_diff_elements(set_1, set_2):
-    return [num for num in set_1 if num not in set_2] + [num for num in set_2 if num not in set_1]
-
-
-def only_diff_elements(set_1, set_2):
-    return (set_1 - set_2) | (set_2 - set_1)
-
-
-def only_diff_elements(set_1, set_2):
-    return set([x for x in set_1 if x not in set_2] + [x for x in set_2 if x not in set_1])
