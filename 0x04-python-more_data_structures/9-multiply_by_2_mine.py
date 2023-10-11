83e754448ac350b79a581b6ba553056d27c8d97b
danielntow <danielntow@gmail.com>
2023-10-11 15:30:32 +0000

    python

---
+++ 0x04-python-more_data_structures/9-multiply_by_2_mine.py
@@ -0,0 +1,13 @@
+def multiply_by_2(a_dictionary):
+    return {key: value * 2 for key, value in a_dictionary.items()}
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
+
+
+def multiply_by_2(a_dictionary):
+    return map(lambda x: y, x, y * 2 for x, y in a_dictionary, a_dictionary )
