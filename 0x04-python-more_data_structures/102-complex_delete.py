04b9d7e7480cf34d01744e0b0ef7b59fa16d89a4
danielntow <danielntow@gmail.com>
2023-10-11 23:43:43 +0000

    python

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -13,3 +13,11 @@ def complex_delete(a_dictionary, value):
 
 def complex_delete(a_dictionary, value):
     return {key: val for key, val in a_dictionary.items() if val != value}
+
+
+# correct one
+def complex_delete(a_dictionary, value):
+    keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
+    for key in keys_to_delete:
+        a_dictionary.pop(key, None)
+    return a_dictionary
