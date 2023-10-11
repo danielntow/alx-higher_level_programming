44810bef4520ed8c62c14e37a871b7e2458b03b7
danielntow <danielntow@gmail.com>
2023-10-11 17:44:37 +0000

    python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
-import roman
 
+def roman_to_int(roman_string):
 
-def roman_to_ints(roman_string):
-    result = roman.fromRoman(roman_string)
-    return result
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_dict[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
--- 0x04-python-more_data_structures/12-roman_to_int_mine.py
+++ 0x04-python-more_data_structures/12-roman_to_int_mine.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman):
+def roman_to_int(roman_string):
+
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
     roman_dict = {
         'I': 1,
         'V': 5,
@@ -14,7 +18,7 @@ def roman_to_int(roman):
     total = 0
     prev_value = 0
 
-    for char in reversed(roman):
+    for char in reversed(roman_string):
         value = roman_dict[char]
         if value < prev_value:
             total -= value
