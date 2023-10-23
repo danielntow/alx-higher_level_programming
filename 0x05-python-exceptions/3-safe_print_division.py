55a4905a532994e39f7824e66633aebe5c98e546
danielntow <danielntow@gmail.com>
2023-10-23 13:10:10 +0000

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        pass
+        result = None
     finally:
         print("Inside result: {}".format(result))
