89c6ef2c66217feb6f4ffd43a8b303d1b2f55438
danielntow <danielntow@gmail.com>
2023-10-23 12:33:39 +0000

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,17 +12,3 @@ def safe_print_list_integers(my_list=[], x=0):
             break
     print()
     return total_count
-
-
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0  # Initialize a counter for the number of integers printed
-    for item in my_list:
-        if count >= x:
-            break  # Exit the loop once x integers are printed
-        try:
-            print("{:d}".format(item), end="")
-            count += 1  # Increment the counter
-        except (ValueError, TypeError):
-            pass  # Skip non-integer values
-    print()  # Print a new line after all integers
-    return count
