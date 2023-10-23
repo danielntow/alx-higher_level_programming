5f8fadfc2ccd384c7b9f4fed2b113c935bae361e
danielntow <danielntow@gmail.com>
2023-10-23 12:33:06 +0000

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    total_count = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                total_count += 1
+            else:
+                continue
+        except IndexError:
+            break
+    print()
+    return total_count
 
 
 def safe_print_list_integers(my_list=[], x=0):
@@ -13,18 +26,3 @@ def safe_print_list_integers(my_list=[], x=0):
             pass  # Skip non-integer values
     print()  # Print a new line after all integers
     return count
-
-
-def safe_print_list_integers(my_list=[], x=0):
-    total_count = 0
-    for i in range(x):
-        try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                total_count += 1
-            else:
-                continue
-        except IndexError:
-            break
-    print()
-    return total_count
