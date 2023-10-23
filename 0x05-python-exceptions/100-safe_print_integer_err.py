7d7f87108f4ca1318d617b55c9d117c8b1897497
danielntow <danielntow@gmail.com>
2023-10-23 14:35:39 +0000

    exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -10,3 +10,13 @@ def safe_print_integer_err(value):
         print("Exception: ", file=sys.stderr, end="")
         print(ne, file=sys.stderr)
         return False
+
+
+def safe_print_integer_err(value):
+    try:
+        integer_value = int(value)
+        print("{:d}".format(integer_value))
+        return True
+    except (ValueError, TypeError):
+        sys.stderr.write("Exception: You must provide an integer\n")
+        return False
