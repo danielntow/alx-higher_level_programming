1ea865403452d1edd4a6d5fc6eaca5e9f9bf1af6
danielntow <danielntow@gmail.com>
2023-10-23 12:51:21 +0000

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -59,7 +59,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
             count += 1
-        print()
     except IndexError:
-        print()
+        pass
+    print()
     return count
