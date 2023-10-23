07bb4f2e625d0c5aed75d6d4f8e08fecff503dad
danielntow <danielntow@gmail.com>
2023-10-23 14:34:44 +0000

    exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python3
-import sys  # Import the sys module to access stderr
+#!/usr/bin/python3'
+import sys
 
 
 def safe_print_integer_err(value):
     try:
-        # Attempt to convert the value to an integer
-        value_as_integer = int(value)
-        print("{:d}".format(value_as_integer))  # Print the integer value
-        return True  # Return True if it's an integer
-    except (ValueError, TypeError):
-        # Handle the exception and print the error message in stderr
-        print("Exception: You have to use an integer", file=sys.stderr)
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as ne:
+        print("Exception: ", file=sys.stderr, end="")
+        print(ne, file=sys.stderr)
         return False
