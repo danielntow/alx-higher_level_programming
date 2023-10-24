52f73d928cef36247219309c5c75ef560f8af790
danielntow <danielntow@gmail.com>
2023-10-24 14:36:10 +0000

    class in python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -22,3 +22,23 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+
+class Square:
+    """
+    A class that defines a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square (default is 0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
