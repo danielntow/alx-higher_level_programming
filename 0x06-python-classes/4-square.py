4ddbc546cfdc3b17b103024444e36f98e0b5ecab
danielntow <danielntow@gmail.com>
2023-10-24 14:59:50 +0000

    classes in python

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,58 +14,6 @@ class Square:
         Initializes a new instance of the Square class.
 
         Args:
-            size (int): The size of the square .
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = size
-
-    def area(self):
-        """
-        Computes the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
-
-    @property
-    def size(self):
-        """retrieve the size of square"""
-
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        set
-        Args:
-            value (int): size of square
-
-        Raises:
-            TypeError:
-            ValueError:
-
-        Returns:
-            int: size of square
-        """
-
-        self.__size = value
-
-
-class Square:
-    """
-    A class that defines a square.
-    """
-
-    def __init__(self, size=0):
-        """
-        Initializes a new instance of the Square class.
-
-        Args:
             size (int, optional): The size of the square (default is 0).
         """
         if not isinstance(size, int):
@@ -87,7 +35,7 @@ class Square:
     @property
     def size(self):
         """
-        Setter method for setting the size attribute.
+        Getter method for setting the size attribute.
 
         Args:
             value (int): The size of the square.
@@ -98,16 +46,10 @@ class Square:
     @size.setter
     def size(self, value):
         """
-        set
+        Setter method for setting the size attribute.
+
         Args:
-            value (int): size of square
-
-        Raises:
-            TypeError:
-            ValueError:
-
-        Returns:
-            int: size of square
+            value (int): The size of the square.
         """
 
         return self.__size == value
