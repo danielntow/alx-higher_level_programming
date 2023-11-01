a1b866851691b3dfed371135b5bd1b4633faf880
danielntow <danielntow@gmail.com>
2023-11-01 15:35:14 +0000

    oop

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" magic string module """
+
+
 def magic_string():
     n = i + 1
     return ', '.join(["BestSchool"] * n)
@@ -10,5 +13,19 @@ def magic_string(my_list=[]):
 
 
 def magic_string():
-    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    """
+    Generates a string containing 'BestSchool' repeated a
+    number of times based on the iteration.
+
+    Returns:
+        str: A string where 'BestSchool' is repeated the
+        number of times corresponding to the iteration number.
+    """
+    # Get the current value of the counter attribute and increment it
+    magic_string.counter = getattr(
+        magic_string, 'counter', 0
+    ) + 1
+
+    # Join 'BestSchool' strings using comma and space based
+    # on the current value of the counter
     return ", ".join(["BestSchool"] * magic_string.counter)
