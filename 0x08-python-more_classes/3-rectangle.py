254a7c4ade400c4afc700804a63d2fde01403457
danielntow <danielntow@gmail.com>
2023-11-01 12:34:55 +0000

    oop

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -142,6 +142,7 @@ class Rectangle:
             return 2 * (self.__width + self.__height)
 
     def __str__(self) -> str:
+        """Returns a string representation of the rectangle."""
         if self.__height == 0 or self.__height == 0:
             return ""
         else:
