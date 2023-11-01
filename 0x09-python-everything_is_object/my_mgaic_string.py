33aa3e685b187568f1f8a21fa83db594e6585383
danielntow <danielntow@gmail.com>
2023-11-01 15:37:27 +0000

    oop

---
+++ 0x09-python-everything_is_object/my_mgaic_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" magic string module """
+
+
+def magic_string():
+    n = i + 1
+    return ', '.join(["BestSchool"] * n)
+
+
+def magic_string(my_list=[]):
+    my_list.append("BestSchool")
+    return ', '.join(my_list)
+
+
+def magic_string():
+    magic_string.counter = getattr(
+        magic_string, 'counter', 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.counter)
