ff386e8bc28df6da907215b7aeb9f3bb8d6e5c4b
danielntow <danielntow@gmail.com>
2023-11-01 10:20:48 +0000

    oop

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,4 +5,15 @@ module that defines a rectangle
 
 
 class Rectangle:
+    """
+    Represents a basic Rectangle.
+
+    Attributes:
+    ----------
+    No attributes defined initially.
+
+    Methods:
+    --------
+    No methods defined initially.
+    """
     pass
