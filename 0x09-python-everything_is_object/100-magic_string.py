0913dbde3c134cb22471742848de05e3f6de66ca
danielntow <danielntow@gmail.com>
2023-11-01 05:29:25 +0000

    everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,3 +2,8 @@
 def magic_string():
     n = i + 1
     return ', '.join(["BestSchool"] * n)
+
+
+def magic_string(my_list=[]):
+    my_list.append("BestSchool")
+    return ', '.join(my_list)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    __slots__ = ('first_name',)
+
+    def __setattr__(self, name, value):
+        if not hasattr(self, 'first_name') and name != 'first_name':
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'")
+        super().__setattr__(name, value)
