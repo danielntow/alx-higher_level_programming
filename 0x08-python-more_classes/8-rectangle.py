892d8d942d4aa59f2748f85b9399eb83bccd42c7
danielntow <danielntow@gmail.com>
2023-11-01 14:40:57 +0000

    oop

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,202 @@
+#!/usr/bin/python3
+"""
+module that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Represents a basic Rectangle.
+
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0) -> None:
+        """
+        Initializes a Rectangle object.
+
+        Parameters:
+        -----------
+        width : int, optional
+            Width of the rectangle (default is 0).
+        height : int, optional
+            Height of the rectangle (default is 0).
+        """
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1  # Increment number_of_instances
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(self.value, int):
+            raise TypeError("width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif self.__height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return '\n'.join(
+                ['#' * self.__width for _ in range(self.__height)])
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return '\n'.join(
+            '#' * self.__width for _ in range(self.__height)
+        ) if self.__width != 0 and self.__height != 0 else ""
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return '\n'.join(
+            ['#' * self.__width for _ in range(self.__height)]
+        ) if self.__width != 0 and self.__height != 0 else ""
+
+    def __str__(self) -> str:
+        """Returns a string representation of the rectangle."""
+        if self.__height == 0 or self.__height == 0:
+            return ""
+        else:
+            return '\n'.join(['#' * self.__width] * self.__height)
+
+    def __del__(self):
+        """Prints a message when an instance of Rectangle is deleted."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+    ----------
+    __width : int
+        Private attribute to store the width of the rectangle.
+    __height : int
+        Private attribute to store the height of the rectangle.
+    """
+
+    number_of_instances = 0  # Public class attribute initialized to 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0) -> None:
+        """
+        Initializes a Rectangle object.
+
+        Parameters:
+        -----------
+        width : int, optional
+            Width of the rectangle (default is 0).
+        height : int, optional
+            Height of the rectangle (default is 0).
+
+        Raises:
+            TypeError: if width is not integer
+            ValueError: if height is less than zero
+        """
+        self.width = width
+        self.height = height
+        self.instances_created()
+
+    @classmethod
+    def instances_created(cls):
+        cls.number_of_instances += 1
+
+    @classmethod
+    def instances_deleted(cls):
+        cls.number_of_instances -= 1
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Claculates Area of Rectangle """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculate and return the perimeter of the rectangle."""
+
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self) -> str:
+        """Returns a string representation of the rectangle."""
+        if self.__height == 0 or self.__height == 0:
+            return str()
+        else:
+            return '\n'.join([
+                str(self.print_symbol) * self.__width
+            ] * self.__height)
+
+    def __repr__(self):
+        """Returns a string representation to recreate a new instance."""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Prints a message when an instance of Rectangle is deleted."""
+        print("Bye rectangle...")
+        self.instances_deleted()
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """" """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        else:
+            if rect_1.area() == rect_2.area():
+                return rect_1
+            elif rect_2.area() > rect_1.area():
+                return rect_2
+            else:
+                return rect_1
