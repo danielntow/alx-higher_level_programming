25f1f834af08cd143ac1e7cc97e14b1c2053532c
danielntow <danielntow@gmail.com>
2023-11-02 04:21:22 +0000

    oop

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,3 +8,10 @@ class LockedClass:
             raise AttributeError(
                 f"'{self.__class__.__name__}' object has no attribute '{name}'")
         super().__setattr__(name, value)
+
+
+class LockedClass:
+    __slots__ = ('first_name',)
+
+    def __init__(self):
+        self.first_name = ""
