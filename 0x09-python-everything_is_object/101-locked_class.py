4da1bee7e97920497cc0657003b1ec3a944966d9
danielntow <danielntow@gmail.com>
2023-11-02 04:27:37 +0000

    oop

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,16 +5,6 @@ Locked Class module
 
 
 class LockedClass:
-    __slots__ = ('first_name',)
-
-    def __setattr__(self, name, value):
-        if not hasattr(self, 'first_name') and name != 'first_name':
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{name}'")
-        super().__setattr__(name, value)
-
-
-class LockedClass:
 
     """
     Class that restrict the dynamically creating
@@ -33,3 +23,26 @@ class LockedClass:
     instance with certain condition
     """
     __slots__ = ["first_name"]
+
+
+class LockedClass:
+    """
+    Class that restrict the dynamically creating
+    instance with certain condition
+    """
+    __slots__ = ('first_name',)
+
+    def __setattr__(self, name, value):
+        """set attr
+
+        Args:
+            name (str): ---
+            value (str: ---
+
+        Raises:
+            AttributeError: ---
+        """
+        if not hasattr(self, 'first_name') and name != 'first_name':
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'")
+        super().__setattr__(name, value)
