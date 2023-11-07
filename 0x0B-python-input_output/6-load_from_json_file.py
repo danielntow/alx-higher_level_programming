a3167df0bdb3aa341a8d2a6ed3763021ad212396
danielntow <danielntow@gmail.com>
2023-11-07 18:02:25 +0000

    python i/o

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module demonstrating python i/o
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    create pythin obj from json file
+
+    Args:
+        filename (str): The name of the file to save the python obj.
+
+    Returns:
+        None.
+    """
+    with open(filename, "w") as file:
+        json.loads(file)
