ae83dd41c242035391a6d99ebccb34f07f61fcf9
danielntow <danielntow@gmail.com>
2023-11-07 14:49:47 +0000

    inheritance in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-
-"""
-This module contains a function to read a text file and print
-its content to the standard output (stdout).
-"""
+"""this module reads from a file"""
 
 
 def read_file(filename=""):
-    """
-    Reads the content of a text file in UTF-8 encoding and
-    prints it to the standard output (stdout).
+    """this functions reads a file"""
 
-    :param filename: (str) The name of the text file.
-    Default is an empty string.
-    :return: None
-    """
-
-    with open(filename, encoding='utf-8') as file:
-        print(file.read())
+    with open(filename, "r", encoding="UTF-8") as file:
+        print(file.read(), end="")
