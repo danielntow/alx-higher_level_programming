67543e12c91898b734f7ce5be5f3c76d1365afbd
danielntow <danielntow@gmail.com>
2023-11-07 15:32:07 +0000

    inheritance in python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""
+mmodule demnstrating python i/o
+"""
+
 
 def write_file(filename="", text=""):
+    """
+    Writes the given string to a text file in UTF-8 encoding.
+
+    Args:
+        filename (str): The name of the file to write the text to.
+        text (str): The string to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
     try:
-        with open(filename, 'w', encoding='utf-8') as file:
-            char_count = file.write(text)
-        return char_count
-    except:
-        return 0
+        with open(filename, "w", encoding="utf-8") as file:
+            characters_written = file.write(text)
+            return characters_written
+    except Exception as e:
+        return 0  # Returning 0 if any exception occurs
