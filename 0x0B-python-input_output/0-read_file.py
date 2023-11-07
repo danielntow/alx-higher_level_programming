216669ed9711c25cad5cded2a0c562b003d83458
danielntow <danielntow@gmail.com>
2023-11-07 14:48:59 +0000

    inheritance in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,8 +15,6 @@ def read_file(filename=""):
     Default is an empty string.
     :return: None
     """
-    try:
-        with open(filename, encoding='utf-8') as file:
-            print(file.read())
-    except FileNotFoundError:
-        pass  # No need to manage file not found exception
+
+    with open(filename, encoding='utf-8') as file:
+        print(file.read())
