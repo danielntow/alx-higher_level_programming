8c03a440a6504be8be2cd1db7d91fd8e3b51e337
danielntow <danielntow@gmail.com>
2023-11-07 17:31:58 +0000

    python i/o

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module demonstrating python i/o
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convert a json string to python obj
+
+    Args:
+    my_str: the json string
+
+    Returns:
+    A python object
+    """
+    json_string = json.loads(my_str)
+    return json_string
