178098d6b80c94b5b1d60fa6ce75797cace6a3f8
danielntow <danielntow@gmail.com>
2023-11-07 04:13:37 +0000

    inheritance in python

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this is a square module
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class inherits from Rectanagle
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Sqaure] {:d}/{:d}".format(self.__size, self.__size)
