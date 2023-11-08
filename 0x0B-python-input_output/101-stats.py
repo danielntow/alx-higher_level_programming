c7d3eaa30ca3a188e87b404288ad96e1e4036e4e
danielntow <danielntow@gmail.com>
2023-11-08 04:46:12 +0000

    python i/o

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -121,3 +121,38 @@ except KeyboardInterrupt:
     for code in sorted(status_codes):
         if status_codes[code] > 0:
             print(f"{code}: {status_codes[code]}")
+
+
+# Initialize variables to store metrics
+total_file_size = 0
+status_code_counts = {200: 0, 301: 0, 400: 0,
+                      401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        try:
+            parts = line.split()
+            if len(parts) >= 9:
+                file_size = int(parts[-1])
+                status_code = int(parts[-2])
+                total_file_size += file_size
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+
+            if i % 10 == 0:
+                print("Total file size: File size: {}".format(total_file_size))
+                for code in sorted(status_code_counts.keys()):
+                    count = status_code_counts[code]
+                    if count > 0:
+                        print("{}: {}".format(code, count))
+                print()
+
+        except (ValueError, IndexError):
+            pass
+
+except KeyboardInterrupt:
+    print("Total file size: File size: {}".format(total_file_size))
+    for code in sorted(status_code_counts.keys()):
+        count = status_code_counts[code]
+        if count > 0:
+            print("{}: {}".format(code, count))
