12d02981c2218ab451e3a2d3fc23f62f2ac82c46
danielntow <danielntow@gmail.com>
2023-11-08 04:44:55 +0000

    python i/o

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -63,3 +63,61 @@ try:
 except KeyboardInterrupt:
     total_size, status_count = process_lines(lines)
     print_statistics(total_size, status_count)
+
+
+status_codes = {"200": 0, "301": 0, "400": 0,
+                "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_file_size = 0
+lines_count = 0
+
+try:
+    for line in sys.stdin:
+        lines_count += 1
+        parts = line.split()
+        if len(parts) > 2:
+            status_code = parts[-2]
+            file_size = int(parts[-1])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            total_file_size += file_size
+
+        if lines_count % 10 == 0:
+            print(f"File size: {total_file_size}")
+            for code in sorted(status_codes):
+                if status_codes[code] > 0:
+                    print(f"{code}: {status_codes[code]}")
+            print()
+except KeyboardInterrupt:
+    print(f"File size: {total_file_size}")
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+
+
+status_codes = {"200": 0, "301": 0, "400": 0,
+                "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_file_size = 0
+lines_count = 0
+
+try:
+    for line in sys.stdin:
+        lines_count += 1
+        parts = line.split()
+        if len(parts) > 2:
+            status_code = parts[-2]
+            file_size = int(parts[-1])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            total_file_size += file_size
+
+        if lines_count % 10 == 0:
+            print(f"File size: {total_file_size}")
+            for code in sorted(status_codes):
+                if status_codes[code] > 0:
+                    print(f"{code}: {status_codes[code]}")
+            print()
+except KeyboardInterrupt:
+    print(f"File size: {total_file_size}")
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
