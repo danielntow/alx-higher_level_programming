d38178ac0ddc474092f924a189487a0edc91709b
danielntow <danielntow@gmail.com>
2023-11-14 05:49:19 +0000

    python almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,46 +1,39 @@
 #!/usr/bin/python3
 import unittest
-from models.base import Base, Rectangle, Square
+import unittest
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestBaseClass(unittest.TestCase):
+class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        # Reset the Base class id counter before each test
+        Base._Base__nb_objects = 0
 
     def test_create_instance(self):
-        base_instance = Base()
-        self.assertIsInstance(base_instance, Base)
-        self.assertEqual(base_instance.id, 1)
+        base = Base()
+        self.assertIsInstance(base, Base)
+        self.assertEqual(base.id, 1)
 
-    def test_to_json_string(self):
-        rectangle = Rectangle(2, 3)
-        square = Square(4)
-        rectangles_json = Rectangle.to_json_string([rectangle.to_dictionary()])
-        squares_json = Square.to_json_string([square.to_dictionary()])
+    def test_create_instance_with_id(self):
+        base = Base(5)
+        self.assertIsInstance(base, Base)
+        self.assertEqual(base.id, 5)
 
-        self.assertEqual(
-            rectangles_json, '[{"id": 1, "width": 2, "height": 3, "x": 0, "y": 0}]')
-        self.assertEqual(
-            squares_json, '[{"id": 2, "size": 4, "x": 0, "y": 0}]')
+    def test_to_json_string_empty_list(self):
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
 
-    def test_load_from_file_csv(self):
-        square1 = Square(5, 1, 2, 3)
-        square2 = Square(8, 4, 5, 6)
-        Square.save_to_file_csv([square1, square2])
+    def test_to_json_string_non_empty_list(self):
+        json_string = Base.to_json_string([{'id': 1, 'x': 2, 'y': 3}])
+        self.assertEqual(json_string, '[{"id": 1, "x": 2, "y": 3}]')
 
-        loaded_squares = Square.load_from_file_csv()
-        self.assertEqual(len(loaded_squares), 2)
-
-        loaded_square1 = loaded_squares[0]
-        loaded_square2 = loaded_squares[1]
-
-        self.assertEqual(loaded_square1.id, 3)
-        self.assertEqual(loaded_square1.size, 5)
-        self.assertEqual(loaded_square1.x, 1)
-        self.assertEqual(loaded_square1.y, 2)
-
-        self.assertEqual(loaded_square2.id, 4)
-        self.assertEqual(loaded_square2.size, 8)
-        self.assertEqual(loaded_square2.x, 4)
-        self.assertEqual(loaded_square2.y, 5)
+    def test_load_from_file_empty(self):
+        instances = Base.load_from_file()
+        self.assertEqual(instances, [])
 
 
 if __name__ == '__main__':
