2ffa7d46315bfcff9d4dfca62a920957e1d5c944
danielntow <danielntow@gmail.com>
2023-12-13 17:18:34 +0000

    more sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,2 @@
 -- Check if the force_name table already exists
-USE hbtn_0d_2;
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL,
-);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
