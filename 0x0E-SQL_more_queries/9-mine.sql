4246233693ecf85f0deba10f8a98c47c7912119b
danielntow <danielntow@gmail.com>
2023-12-14 05:09:56 +0000

    more sql

--- 0x0E-SQL_more_queries/9-mine.sql
+++ 0x0E-SQL_more_queries/9-mine.sql
@@ -1,10 +1,3 @@
--- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE states.id=state_id
-ORDER BY cities.id
-
-/*
 -- Assuming the database name is passed as an argument, use the specified database
 USE hbtn_0d_usa;
 
@@ -13,4 +6,3 @@ SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
-*/
