030afb6677e26858a43581a07c2e791b89958ebc
danielntow <danielntow@gmail.com>
2023-12-14 05:07:29 +0000

    more sql

--- 0x0E-SQL_more_queries/8-mine.sql
+++ 0x0E-SQL_more_queries/8-mine.sql
@@ -2,7 +2,7 @@
 USE hbtn_0d_usa;
 
 -- Find the id of California in the states table
-SET @california_id = (SELECT id FROM states WHERE name = 'California');
+SET @california_id = (SELECT id FROM states WHERE name = 'California' LIMIT 1);
 
 -- List all the cities of California, sorted in ascending order by cities.id
 SELECT cities.id, cities.name
