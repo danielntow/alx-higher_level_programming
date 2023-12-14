71c44db57f16187991dab0936de92345176b3d73
danielntow <danielntow@gmail.com>
2023-12-14 05:13:49 +0000

    more sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,19 +1,8 @@
 -- Assuming the database name is passed as an argument, use the specified database
 USE hbtn_0d_usa;
 
--- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE states.id=state_id
-ORDER BY cities.id ASC
-
-/*
--- Assuming the database name is passed as an argument, use the specified database
-USE hbtn_0d_usa;
-
 -- List all cities with their corresponding state names
 SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
-*/
