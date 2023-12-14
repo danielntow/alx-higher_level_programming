419d8524757ca0165d60085b8efaa17990632a15
danielntow <danielntow@gmail.com>
2023-12-14 05:05:49 +0000

    more sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Assuming the database name is passed as an argument, use the specified database
+USE hbtn_0d_usa;
+
+-- List all cities with their corresponding state names
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
