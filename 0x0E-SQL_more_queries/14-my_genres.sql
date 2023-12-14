6d6c5598e65b092b0260f38e64f4b7ca492acf7a
danielntow <danielntow@gmail.com>
2023-12-14 08:32:26 +0000

    more sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,5 @@
 -- Switch to the hbtn_0d_tvshows database
 USE hbtn_0d_tvshows;
-
--- Select the genre names from tv_genres for the show Dexter
--- Use INNER JOIN to match the genre of Dexter with the genres in tv_genres
--- Order the results in ascending order by the genre name
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
