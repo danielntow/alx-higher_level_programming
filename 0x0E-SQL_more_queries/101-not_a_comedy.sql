0be25a44191c4b247263915deff41be8ff31bcb6
danielntow <danielntow@gmail.com>
2023-12-14 10:18:10 +0000

    more sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,15 @@
+-- Switch to the hbtn_0d_tvshows database
+USE hbtn_0d_tvshows;
+
+-- Get the genre_id for Comedy
+SET @comedy_genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy');
+
+-- Select all shows without the Comedy genre
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT show_id
+    FROM tv_show_genres
+    WHERE genre_id = @comedy_genre_id
+)
+ORDER BY title ASC;
