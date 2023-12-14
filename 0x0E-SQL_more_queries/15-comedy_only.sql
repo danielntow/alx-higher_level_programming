503b8071da25864e8726480ba3ee18104286ac87
danielntow <danielntow@gmail.com>
2023-12-14 08:31:42 +0000

    more sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,4 @@
 -- Switch to the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
--- Select the show titles from tv_shows that have the
--- genre Comedy
--- Use INNER JOIN to match the genre Comedy with
--- the genres in tv_genres
--- Order the results in ascending order by the show title
 SELECT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres
