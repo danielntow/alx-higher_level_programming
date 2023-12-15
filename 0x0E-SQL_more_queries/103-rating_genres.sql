9f9773198ad397d3b707ac736d540607b4ba9687
danielntow <danielntow@gmail.com>
2023-12-15 05:45:18 +0000

    more sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- mysql
-SELECT tv_genres.name, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating_sum
+SELECT tv_genres.name, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
