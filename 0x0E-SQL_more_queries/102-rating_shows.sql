ecc366b70f6eb4ba4e7c43fee29d2a723bb1bbc8
danielntow <danielntow@gmail.com>
2023-12-15 05:36:25 +0000

    more sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,4 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating
-USE hbtn_0d_tvshows_rate;
 SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating_sum
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
