5ab9ff722abc0d3685377171ee7b160489aa875c
danielntow <danielntow@gmail.com>
2024-01-08 15:43:30 +0000

    js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  return a + b;
-}
+// function add (a, b) {
+//   return a + b
+// }
 
 const newFunc = (a, b) => {
   return a + b;
 };
 
-const checkFunc = function (a, b) {
-  return result;
-};
+// const checkFunc = function (a, b) {
+//   return result
+// }
 
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
 const result = newFunc(arg1, arg2);
-console.log(result);
+
+console.log(result); // print result
