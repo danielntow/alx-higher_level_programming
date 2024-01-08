58968d3ee71f254c35d4539f264b2bb1767cc68d
danielntow <danielntow@gmail.com>
2024-01-08 13:18:22 +0000

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-
-// Storing lines in variables and printing them
-const line1 = 'C is fun';
-const line2 = 'Python is cool';
-const line3 = 'JavaScript is amazing';
-
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
