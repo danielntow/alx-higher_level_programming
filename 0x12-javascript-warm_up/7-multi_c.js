df3e514c99ccc1e6a5a57565aaaadcb0cbd6aa02
danielntow <danielntow@gmail.com>
2024-01-08 14:41:06 +0000

    js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-const arg = process.argv[2];
 
-let i = 0;
-while (i < arg) {
-  console.log('C is fun');
-  i++;
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
