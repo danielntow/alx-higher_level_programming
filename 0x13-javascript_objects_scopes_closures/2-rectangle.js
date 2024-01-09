85c5b4739aa974037115eed4f5124ed0b49e4e87
danielntow <danielntow@gmail.com>
2024-01-09 12:36:40 +0000

    js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
   constructor (w, h) {
     if (!w || !h || w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
       // Create an empty object
-      return {};
+      return;
     } else {
       this.width = w;
       this.height = h;
