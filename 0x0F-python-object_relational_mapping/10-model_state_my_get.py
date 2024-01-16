22840a1f2ae855e7d94fdb31ac99ab0d4a8bd034
danielntow <danielntow@gmail.com>
2024-01-16 02:21:04 +0000

    python orm

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain the letter a
-   from the database hbtn_0e_6_usa
+"""script that prints the State object with the name
+   passed as argument
+   from the database hbtn_0e_6_usa (SQL injection free)
 """
 
 import sys
@@ -10,12 +11,13 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database>\
+        <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
     # Assign command line arguments to variables
-    username, password, database = sys.argv[1:]
+    username, password, database, state_name = sys.argv[1:]
 
     try:
         # Create the engine and bind it to the Base class
@@ -26,12 +28,14 @@ if __name__ == "__main__":
         # Create a session to interact with the database
         Session = sessionmaker(bind=engine)
         with Session() as session:
-            # Query and retrieve all State objects containing the letter 'a'
-            states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+            # Query and retrieve the State object with the given name
+            state = session.query(State).filter_by(name=state_name).first()
 
             # Display results
-            for state in states:
-                print("{}: {}".format(state.id, state.name))
+            if state:
+                print("{}".format(state.id))
+            else:
+                print("Not found")
 
     except Exception as e:
         print("Error: {}".format(e))
