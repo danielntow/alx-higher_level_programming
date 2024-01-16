120d833dd558864e54958a0abdf4ebff053e4b27
danielntow <danielntow@gmail.com>
2024-01-16 02:04:05 +0000

    python orm

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""script that lists all State objects that contain the letter a
+   from the database hbtn_0e_6_usa using regexp
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Check if all arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Assign command line arguments to variables
+    username, password, database = sys.argv[1:]
+
+    try:
+        # Create the engine and bind it to the Base class
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+
+        # Create a session to interact with the database
+        Session = sessionmaker(bind=engine)
+        with Session() as session:
+            # Query and retrieve all State objects using regexp to match 'a'
+            states = session.query(State).filter(
+                State.name.op('REGEXP')('[aA]')
+                ).order_by(State.id).all()
+
+            # Display results
+            for state in states:
+                print("{}: {}".format(state.id, state.name))
+
+    except Exception as e:
+        print("Error: {}".format(e))
+        sys.exit(1)
