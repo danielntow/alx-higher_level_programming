a76cb333aff38eb43590bc5fb23ee58cb11e177b
danielntow <danielntow@gmail.com>
2024-01-16 11:13:10 +0000

    python orm

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,48 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects, and corresponding City
-objects, contained in the database hbtn_0e_101_usa
-"""
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""script that lists all State objects, and corresponding City
+ objects, contained in the database hbtn_0e_101_usa"""
+from sqlalchemy import (create_engine)
 from relationship_state import Base, State
 from relationship_city import City
-
-
-def list_states_cities(username, password, database):
-    """List all State objects and corresponding City objects."""
-    # Create an SQLAlchemy engine
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
-        pool_pre_ping=True)
-
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query and retrieve all State objects, ordered by id
-    states = session.query(State).order_by(State.id)
-
-    # Display results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
-
-    # Close the session
-    session.close()
+from sqlalchemy.orm import sessionmaker
+import sys
 
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
-        sys.exit(1)
-
-    # Assign command line arguments to variables
-    username, password, database = sys.argv[1:4]
-
-    # Call the function with the provided arguments
-    list_states_cities(username, password, database)
+    """Connecting"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).order_by(State.id)
+    for st in result:
+        print("{}: {}".format(st.id, st.name))
+        for ct in st.cities:
+            print("\t{}: {}".format(ct.id, ct.name))
+    session.close()
