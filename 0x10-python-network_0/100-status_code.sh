3b4d8510e0062ecb0b76a2b9549f2383f74622a7
danielntow <danielntow@gmail.com>
2024-01-31 02:14:13 +0000

    status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays only the status code of the response
-
-url="$1"
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-echo "$status_code"
+url="$1" status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url") && echo "$status_code"
