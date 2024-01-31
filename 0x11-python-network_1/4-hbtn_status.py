d9dfa3c462b9f3daff3b260e7bf572db2d5089dd
danielntow <danielntow@gmail.com>
2024-01-31 05:57:43 +0000

    requet status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-Script documentation: Fetches the status from
-https://alx-intranet.hbtn.io/status using the requests package.
-"""
+Write a Python script that fetches https://intranet.hbtn.io/status
 
+"""
 import requests
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
 
-    # Sending a GET request to the specified URL
-    response = requests.get(url)
-    # Raise an exception for HTTP errors (4xx or 5xx)
-    response.raise_for_status()
-
-    # Displaying the body of the response with tabulation
-    print("- Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    url = "https://intranet.hbtn.io/status"
+    req = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
