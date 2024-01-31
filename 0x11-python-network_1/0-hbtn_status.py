c71a3bdbd31761eae6f1af98e6ef6e364060033d
danielntow <danielntow@gmail.com>
2024-01-31 02:34:30 +0000

    fetche status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,10 +10,10 @@ url = "https://alx-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
     # Reading and decoding the body of the response
-    content = response.read().decode('utf-8')
+    content = response.read()
 
     # Displaying the body of the response with tabulation
     print("- Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
-    print('\t- utf8 content: {}'.format(content))
+    print('\t- utf8 content: {}'.format(content.decode("UTF-8")))
