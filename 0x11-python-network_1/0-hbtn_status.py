0796c57a0e357b722ee2eaabf6863164c6555646
danielntow <danielntow@gmail.com>
2024-01-31 02:38:46 +0000

    fetche status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,15 +6,29 @@ https://alx-intranet.hbtn.io/status using urllib.
 
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
 
+def fetch_status(url):
+    """
+    Fetches the status from the specified URL using urllib.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        str: The content of the response.
+    """
     with urllib.request.urlopen(url) as response:
         # Reading and decoding the body of the response
         content = response.read().decode('utf-8')
+        return content
 
-        # Displaying the body of the response with tabulation
-        print("- Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print('\t- utf8 content: {}'.format(content))
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    status_content = fetch_status(url)
+
+    # Displaying the body of the response with tabulation
+    print("- Body response:")
+    print("\t- type: {}".format(type(status_content)))
+    print("\t- content: {}".format(status_content))
+    print('\t- utf8 content: {}'.format(status_content))
