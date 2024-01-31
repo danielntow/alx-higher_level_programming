660f664706059c1b5d1d7a8f9e67ff4669c86065
danielntow <danielntow@gmail.com>
2024-01-31 02:28:08 +0000

    fetche status

---
+++ 0x10-python-network_0/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script documentation: Fetches the status from
+https://alx-intranet.hbtn.io/status using urllib.
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    # Reading and decoding the body of the response
+    content = response.read().decode('utf-8')
+
+    # Displaying the body of the response with tabulation
+    print("- Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
