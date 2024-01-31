dc091e9409c74b6eaa56229886208b99686db99c
danielntow <danielntow@gmail.com>
2024-01-31 05:31:26 +0000

    last 10 commits

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL, sends a
-request to the URL and displays the body of the response.
+GitHub API Script: Display user id using Basic Authentication.
 """
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = "https://api.github.com/user"
-    session = requests.Session()
-    session.auth = (argv[1], argv[2])
-    response = session.get(url)
-    try:
-        print('{}'.format(response.json()['id']))
-    except Exception as e:
-        print("None")
+    # Extracting command line arguments
+    username = argv[1]
+    password = argv[2]
+
+    # GitHub API endpoint for authenticated user
+    api_url = "https://api.github.com/user"
+
+    # Sending GET request to GitHub API with Basic Authentication
+    response = requests.get(api_url, auth=(username, password))
+
+    # Raise an exception for HTTP errors (4xx or 5xx)
+    response.raise_for_status()
+
+    # Extracting and printing the user id
+    user_id = response.json().get('id')
+    print(f"User ID: {user_id}")
