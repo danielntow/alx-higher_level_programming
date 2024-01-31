37003346a9df4d4434d48ae9a56cbd7d0a25304c
danielntow <danielntow@gmail.com>
2024-01-31 02:15:04 +0000

    status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays only the status code of the response
-url="$1" status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url") && echo "$status_code"
+# Write a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response. FYI: -s = Don't show download progress, -o /dev/null = don't display the body, -w "%{http_code}" = Write http response code to stdout after exit.
+curl -s -o /dev/null -w "%{http_code}" "$1"
