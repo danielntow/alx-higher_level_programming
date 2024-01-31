94b52b4f730a36db13d4f93a78e9f9817547f669
danielntow <danielntow@gmail.com>
2024-01-31 05:04:30 +0000

    request correct error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""check the error code"""
-
+"""
+Write a Python script that takes in a URL, sends a
+request to the URL and displays the body of the response.
+"""
 import requests
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    if req.status_code is not requests.code.ok:
-        print("Error code: {}".format(req.status_code))
+    if req.status_code >= 400:
+        print('Error code: {}'.format(req.status_code))
     else:
         print(req.text)
