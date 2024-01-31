40de397b346c993edda1ddb3a5d273e7116a8fc7
danielntow <danielntow@gmail.com>
2024-01-31 02:40:27 +0000

    fetche status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
 """
-Script documentation: Fetches the status from
-https://alx-intranet.hbtn.io/status using urllib.
+Write a Python script that fetches https://intranet.hbtn.io/status
 """
-
 import urllib.request
 
-
-def fetch_status(url):
-    """
-    Fetches the status from the specified URL using urllib.
-
-    Args:
-        url (str): The URL to fetch.
-
-    Returns:
-        str: The content of the response.
-    """
-    with urllib.request.urlopen(url) as response:
-        # Reading and decoding the body of the response
-        content = response.read().decode('utf-8')
-        return content
-
-
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    status_content = fetch_status(url)
-
-    # Displaying the body of the response with tabulation
-    print("- Body response:")
-    print("\t- type: {}".format(type(status_content)))
-    print("\t- content: {}".format(status_content))
-    print('\t- utf8 content: {}'.format(status_content))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode("UTF-8")))
