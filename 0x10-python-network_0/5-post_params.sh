be729d7bbaf0c462bdbe51b4f5bb2d1590235654
danielntow <danielntow@gmail.com>
2024-01-31 01:38:08 +0000

    python networking

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes a URL, sends a POST request with variables, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
