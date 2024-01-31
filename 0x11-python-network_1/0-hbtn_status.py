85fc7014cbe87e31ca3b0f299515889e20339c49
danielntow <danielntow@gmail.com>
2024-01-31 02:33:03 +0000

    fetche status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -16,3 +16,4 @@ with urllib.request.urlopen(url) as response:
     print("- Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
+    print('\t- utf8 content: {}'.format(content))
