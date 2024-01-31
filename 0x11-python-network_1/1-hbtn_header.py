cae39fa796a9fb82b64f25026e9d83fdd4efc665
danielntow <danielntow@gmail.com>
2024-01-31 02:50:30 +0000

    fetche status

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
 """
-Uses the urllib and sys modules to send a request to a URL and display the X-Request-Id value.
+Script documentation: Sends a request to a URL
+and displays the value of the X-Request-Id variable
+in the response header.
 """
-
-import urllib.request as req
+import urllib.request
 import sys
 
 if __name__ == "__main__":
-    # Check if a URL is provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
-    url_to_fetch = sys.argv[1]
-
-    try:
-        # Sending a request to the specified URL
-        with req.urlopen(url_to_fetch) as response:
-            # Checking if the 'X-Request-Id' header is present
-            if 'X-Request-Id' in response.headers:
-                x_request_id = response.headers['X-Request-Id']
-                print(x_request_id)
-            else:
-                print("X-Request-Id not found in the response headers.")
-    except Exception as e:
-        print("Error:", e)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('x-request-id'))
